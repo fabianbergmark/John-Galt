37c2f4a07164e1a88a054f33f0837a7f4927bafa
Johan Stenberg <johanstenberg92@gmail.com>
2013-11-01 14:01:40 +0100

    Add calendar.js helper for finding free periods and add Johan Stenberg as collaborator.

--- settings.js
+++ settings.js
@@ -8,3 +8,6 @@ exports.sqlite3 =
 exports.constants =
   { "book": { "time_limit": 6,
               "confirmation_period": 15 * 60 * 1000 } };
+
+exports.ical =
+  { "url" : "https://www.kth.se/social/user/163518/icalendar/9af14e82d8a1f778fd6b63348bc6ff221d7f127f" }
